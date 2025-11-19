e70403949cb1b8bca0872a5c3cc95cab54e7644d
shreyagg304 <154610438+shreyagg304@users.noreply.github.com>
2025-11-19 20:05:09 +0530

    Delete wellnessApi.js

--- wellnessApi.js
+++
@@ -1,7 +0,0 @@
-import api from "./client";
-
-export const submitDailyWellness = (data) =>
-  api.post("/api/wellness", data);
-
-export const getWellnessHistory = () =>
-  api.get("/api/wellness/history");
